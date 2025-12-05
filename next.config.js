34201a1e939093b20e570feb400e88c26d2f9b73
Erisson Nogueira <erisson.eduardo@gmail.com>
2025-12-05 01:05:37 -0300

    Fix Permissions-Policy to allow camera and microphone

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ const securityHeaders = [
   },
   {
     key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=()'
+    value: 'camera=(self), microphone=(self), geolocation=()'
   }
 ]
 
